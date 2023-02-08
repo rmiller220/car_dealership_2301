6f6d986d89e7e6f90012c66e8d89f888b7ef92e2
Reilly Miller <reillyjmiller31@gmail.com>
2023-02-08 10:58:08 -0700

    create methods for car class

--- lib/car.rb
+++ lib/car.rb
@@ -1,5 +1,28 @@
 class Car
-
+  attr_reader :make_model, :payment, :loan_length
+  attr_accessor :color
   def initialize(make_model, payment, loan_length)
+    @make_model = make_model
+    @payment = payment
+    @loan_length = loan_length
+    @color = nil
+  end
+  
+  def make
+    @make_model.split[0]
+  end
+  
+  def model
+    @make_model.split[1]
+  end
+
+  def monthly_payment
+    @payment
+  end
+  def total_cost
+    @payment*@loan_length
+  end
+  def paint!(color)
+    @color = color
   end
 end
